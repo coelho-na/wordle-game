272ce8776778e4e692cdd92a6552de29c9d5c75d
coelho-na <nadinecoelho9@gmail.com>
2022-06-01 23:52:48 -0300

    cathing solutions word

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
+  return (
+    <div className="App">
+      <h1>Wordle</h1>
+      {<div>Solution is: {solution}</div>}
+    </div>
+  );
+}
+
+export default App;
