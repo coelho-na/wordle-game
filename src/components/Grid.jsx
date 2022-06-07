9b1b0741b1871561626e0ad696de591dcc782069
coelho-na <nadinecoelho9@gmail.com>
2022-06-06 23:46:31 -0300

    showing guesses in the rows

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -5,7 +5,7 @@ const Grid = ({ currentGuess, guesses, turn }) => {
   return (
     <div>
       {guesses.map((guess, index) => {
-        return <Row key={index} />;
+        return <Row key={index} guess={guess} />;
       })}
     </div>
   );
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 
-const Row = () => {
+const Row = ({ guess }) => {
+  if (guess) {
+    return (
+      <div className="row past">
+        {guess.map((letter, index) => (
+          <div key={index} className={letter.color}>
+            {letter.key}
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <div className="row">
       <div></div>
