35000e8fa100dd83a638b2bce57504d371bdd320
Nadine Coelho <nadinecoelho9@gmail.com>
2022-06-06 21:30:05 -0300

    listen to key up event and add guesses to history

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import { useState, useEffect } from "react";
+
 import "./App.css";
+import Wordle from "./components/Wordle";
 
 function App() {
   const [solution, setSolution] = useState(null);
 
   useEffect(() => {
-    fetch("http://localhost:3001/solutions")
-      .then((res) => res.json())
-      .then((json) => {
-        const randomSolution = json[Math.floor(Math.random() * json.length)];
-        setSolution(randomSolution.word);
-      });
+    getData();
   }, [setSolution]);
+
+  const getData = async () => {
+    const response = await fetch(
+      "https://my-wordle-api.herokuapp.com/solutions"
+    );
+    const data = await response.json();
+    const randomSolution = data[Math.floor(Math.random() * data.length)];
+    console.log(randomSolution);
+  };
 
   return (
     <div className="App">
       <h1>Wordle</h1>
-      {<div>Solution is: {solution}</div>}
+      {<Wordle solution={solution} />}
     </div>
   );
 }
